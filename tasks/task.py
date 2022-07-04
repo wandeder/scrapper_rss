cba71704f3efe10d83dd8b84399af398912c3f08
Aitemir Kuandyk <aitemir.kuandyk@alcon.com>
2022-07-05 02:23:07 +0600

    Initial commit for black box approach for testing

---
+++ tasks/task.py
@@ -0,0 +1,13 @@
+# You shouldn't change signatures of the initial functions,
+# and should use modules that were initially imported.
+
+# =============================================================================
+# DO NOT TOUCH THE FOLLOWING CODE
+from typing import Optional, Sequence
+from argparse import ArgumentParser
+
+# =============================================================================
+
+
+def main(argv: Optional[Sequence] = None):
+    """Your code goes here ..."""
