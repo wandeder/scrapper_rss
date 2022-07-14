a552a9bf145b610db5eee682ee2958c4c52319c9
Aitemir Kuandyk <aitemir.kuandyk@alcon.com>
2022-07-14 14:30:37 +0600

    added requirements and fix template

--- tasks/rss_reader.py
+++ tasks/rss_reader.py
@@ -1,7 +1,7 @@
 # You shouldn't change  name of function or their arguments
 # but you can change content of the initial functions.
 from argparse import ArgumentParser
-from typing import List, Optional, Sequence, Union, cast
+from typing import List, Optional, Sequence
 import requests
 
 
@@ -10,7 +10,7 @@ class UnhandledException(Exception):
 
 
 def rss_parser(
-    xmls: List[str],
+    xml: str,
     limit: Optional[int] = None,
     json: bool = False,
 ) -> List[str]:
@@ -18,7 +18,7 @@ def rss_parser(
     RSS parser.
 
     Args:
-        xmls: List of XML strings.
+        xml: XML document as a string.
         limit: Number of the news to return. if None, returns all news.
         json: If True, format output as JSON.
 
@@ -27,8 +27,8 @@ def rss_parser(
         Which then can be printed to stdout or written to file as a separate lines.
 
     Examples:
-        >>> xmls = ['<rss><channel><title>Some RSS Channel</title><link>https://some.rss.com</link><description>Some RSS Channel</description></channel></rss>']
-        >>> rss_parser(xmls)
+        >>> xml = '<rss><channel><title>Some RSS Channel</title><link>https://some.rss.com</link><description>Some RSS Channel</description></channel></rss>'
+        >>> rss_parser(xml)
         ["Feed: Some RSS Channel",
         "Link: https://some.rss.com"]
         >>> print("\\n".join(rss_parser(xmls)))
@@ -59,9 +59,9 @@ def main(argv: Optional[Sequence] = None):
     )
 
     args = parser.parse_args(argv)
-    xmls = [requests.get(src).text for src in args.source]
+    xml = requests.get(args.source).text
     try:
-        print("\n".join(rss_parser(xmls, args.limit, args.json)))
+        print("\n".join(rss_parser(xml, args.limit, args.json)))
         return 0
     except Exception as e:
         raise UnhandledException(e)
